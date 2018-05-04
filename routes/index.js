b6f113278a059afeef2ac33c1cd74cf395a183d6
sergiolopezlopez <s.llopez@alumnos.upm.es>
2018-05-05 00:27:18 +0200

    version3

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+const model = require("../models/index");
 const Sequelize = require('sequelize');
-const sequelize = require("../models/index");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Quiz' });
 });
 
+
 router.get('/credits', function(req, res, next) {
-    res.render('credits', { title:'Quiz', name: 'Alvaro Garnica Navarro' });
+    res.render('credits', { title:'Credito', autor: 'Sergio López López' });
 });
 
 router.get('/quizzes', function(req, res, next) {
-    sequelize.models.quiz.findAll().
+    model.models.quiz.findAll().
     then( quizzes => {
         res.render('quizzes', {quizzes});
-    })
-        .catch(Sequelize.ValidationError, error => {
-
+    }).catch(Sequelize.ValidationError, error => {
         })
         .catch(error => {
         });
